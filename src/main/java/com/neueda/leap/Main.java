75ab078ee17c1e2186ce1921a6ed4447d2825552
EC2 Default User <ec2-user@ip-10-18-78-115.eu-west-1.compute.internal>
2026-08-20 15:10:36 +0000

    move in starter folder from lab 13

---
+++ src/main/java/com/neueda/leap/Main.java
@@ -0,0 +1,8 @@
+package com.neueda.leap;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO: replace <team-name> with your team's actual name
+        System.out.println("Hello world from the firm's Sprint 1 project skeleton");
+    }
+}
