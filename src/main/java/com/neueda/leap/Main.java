a3e1faa411b5097fc3d5cfee28f4f2943558c301
EC2 Default User <ec2-user@ip-10-18-78-115.eu-west-1.compute.internal>
2026-08-20 15:15:14 +0000

    edit to our team name

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -3,6 +3,6 @@ package com.neueda.leap;
 public class Main {
     public static void main(String[] args) {
         // TODO: replace <team-name> with your team's actual name
-        System.out.println("Hello world from the firm's Sprint 1 project skeleton");
+        System.out.println("Hello world from the Fintech Five's Sprint 1 project skeleton");
     }
 }
