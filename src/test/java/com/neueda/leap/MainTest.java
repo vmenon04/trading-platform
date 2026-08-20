b1021344550be121ba1636b181a2a6544b98f850
EC2 Default User <ec2-user@ip-10-18-78-115.eu-west-1.compute.internal>
2026-08-20 18:02:34 +0000

    import Main

--- src/test/java/com/neueda/leap/MainTest.java
+++ src/test/java/com/neueda/leap/MainTest.java
@@ -2,6 +2,7 @@ package com.neueda.leap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import com.neueda.leap.Main;
 
 public class MainTest {
 
