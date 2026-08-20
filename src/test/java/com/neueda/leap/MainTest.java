ecfa9d39d517216092d6ab037f5335bb34950a5f
EC2 Default User <ec2-user@ip-10-18-78-115.eu-west-1.compute.internal>
2026-08-20 17:52:53 +0000

    add main test

---
+++ src/test/java/com/neueda/leap/MainTest.java
@@ -0,0 +1,13 @@
+package com.neueda.leap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    public void testMain() {
+    assertEquals("Hello world from the Fintech Five's Sprint 1 project skeleton", main());
+    }
+
+}
