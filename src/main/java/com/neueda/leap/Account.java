bf7676258601b87aada318a13b9a10481362b2c3
Archana Goli <anniegoli123@gmail.com>
2026-09-17 19:22:02 +0000

    added skeleton of classes

---
+++ src/main/java/com/neueda/leap/Account.java
@@ -0,0 +1,6 @@
+package com.neueda.leap;
+
+public class Account {
+    private int account_id;
+    private String account_type;
+}
---
+++ src/main/java/com/neueda/leap/ModelPortfolio.java
@@ -0,0 +1,6 @@
+package com.neueda.leap;
+
+public class ModelPortfolio {
+    private int model_portfolio_id;
+    private String name;
+}
