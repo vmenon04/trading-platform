6f3c7322d7cc170a5a5fc8cfe8db14835368f40a
Vasudev Menon <vmenon2@ncsu.edu>
2026-09-24 14:43:47 -0400

    Added OrderManagement skeleton

--- src/main/java/com/neueda/leap/service/OrderManagementService.java
+++ src/main/java/com/neueda/leap/service/OrderManagementService.java
@@ -1,8 +1,26 @@
 package com.neueda.leap.service;
 
+import com.neueda.leap.dto.OrderRequest;
+import com.neueda.leap.repository.AccountTradeMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderManagementService {
-    
+
+    private final InstrumentService instrumentService;
+    private final ValidationService validationService;
+    private final ExecutionService executionService;
+    private final AccountTradeMapper accountTradeMapper;
+
+    public OrderManagementService(InstrumentService instrumentService, ValidationService validationService,
+                                  ExecutionService executionService, AccountTradeMapper accountTradeMapper) {
+        this.instrumentService = instrumentService;
+        this.validationService = validationService;
+        this.executionService = executionService;
+        this.accountTradeMapper = accountTradeMapper;
+    }
+
+    public synchronized int placeOrder(OrderRequest order) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
