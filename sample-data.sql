099da274ed9b1e488b75652cbde355b5ce2b944a
Vasudev Menon <vmenon2@ncsu.edu>
2026-09-25 15:30:48 -0400

    Update email creation logic in sample data creation

--- sample-data.sql
+++ sample-data.sql
@@ -39,7 +39,7 @@ last_names AS (
     ('Stevens'), ('Tucker'), ('Porter'), ('Hunter'), ('Hicks'), ('Crawford'), ('Henry'), ('Boyd')
   ) AS t(name)
 )
-SELECT fn.name, ln.name, LOWER(fn.name || '.' || ln.name || '@example.com'), DATE '1950-01-01' + (random() * 25000)::INT
+SELECT fn.name, ln.name, LOWER(fn.name || '.' || ln.name) || '.' || ROW_NUMBER() OVER () || '@example.com', DATE '1950-01-01' + (random() * 25000)::INT
 FROM first_names fn
 CROSS JOIN last_names ln
 LIMIT 1000;
